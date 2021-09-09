c0837410bf969de5d0b5d68d205ee3fdfe04f124
QAfun <arthur.popko@icloud.com>
2021-09-09 12:14:49 +0300

    tests passed video added

--- cypress/all_tests/ui_tests/ui_test.js
+++ cypress/all_tests/ui_tests/ui_test.js
@@ -3,16 +3,22 @@ import SearchResultsPage from "../../page_objects/searchResultsPage";
 import AccessoriesPage from "../../page_objects/accessoriesPage";
 
 describe('UI test for google store', () => {
+    before( () => {
+        cy.fixture('product').then(data => {
+            cy.wrap(data).as('productData')
+        })
+    })
 
     it('Positive: find the item by name in google store', () => {
-        cy.log("GIVEN User is at AccessoriesPage")
-        AccessoriesPage.open()
+        cy.get('@productData').then((productData) => {
+            cy.log("GIVEN User is at AccessoriesPage")
+            AccessoriesPage.open()
 
-        cy.log("When User perform search product by name")
-        AccessoriesPage.performSearch('Google Pixel')
+            cy.log("When User perform search product by name")
+            AccessoriesPage.performSearch(productData.name)
 
-        SearchResultsPage.getProductByDocId("pixel_5")
-            .should("exist")
+            SearchResultsPage.getProductByDocId(productData.url)
+                .should("exist")
+        })
     })
   })
-
